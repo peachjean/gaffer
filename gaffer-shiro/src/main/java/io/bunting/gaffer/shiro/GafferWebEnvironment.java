c0716956b60dae1d09352f3e7b3841cd59ab0f6c
Jared Bunting <jared.bunting@peachjean.com>
2015-03-10 05:54:46 -0500

    Adding initial shiro module structure.

--- gaffer-shiro/src/main/java/io/bunting/gaffer/shiro/GafferWebEnvironment.java
+++ gaffer-shiro/src/main/java/io/bunting/gaffer/shiro/GafferWebEnvironment.java
@@ -1,8 +1,40 @@
 package io.bunting.gaffer.shiro;
 
+import io.bunting.gaffer.Gffr;
+
+import org.apache.shiro.ShiroException;
+import org.apache.shiro.util.Destroyable;
+import org.apache.shiro.util.Initializable;
+import org.apache.shiro.web.env.IniWebEnvironment;
+import org.apache.shiro.web.env.ResourceBasedWebEnvironment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * TODO: Document this class
  */
-class GafferWebEnvironment
+public class GafferWebEnvironment extends ResourceBasedWebEnvironment implements Initializable, Destroyable
 {
+	public static final String DEFAULT_WEB_GAFFER_RESOURCE_PATH = "/WEB-INF/shiro.gfr";
+
+	private static final Logger log = LoggerFactory.getLogger(IniWebEnvironment.class);
+
+	private Gffr gaffer;
+
+	@Override
+	public void init() throws ShiroException
+	{
+		final Gffr gffr = getGaffer();
+//		gffr.loadConfiguredObjects();
+	}
+
+	public Gffr getGaffer()
+	{
+		return gaffer;
+	}
+
+	public void setGaffer(final Gffr gaffer)
+	{
+		this.gaffer = gaffer;
+	}
 }
